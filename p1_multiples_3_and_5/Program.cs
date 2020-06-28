69779dc2253ad22b61721f0eacf3d6c498bf4fc9
yousufhassan <yousuf_hassan20@yahoo.ca>
2020-06-27 23:43:37 -0400

    Add first euler problem to repository Changes: - created an ssh key for main laptop - created new directory for project-euler repository - moved first problem folder inside local repository directory - added first problem repository

---
+++ p1_multiples_3_and_5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+Namespace: p1_multiples_3_and_5
+Class: Project Euler
+ClassName: Program
+Description: This program finds the sum of all the multiples of 3 or 5 below 1000.
+Date: May 4, 2020
+Notes:
+*/
+
+namespace p1_multiples_3_and_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0)) // if the number is a multiple of 3 or 5
+                {
+                    sum += i; // add it to the sum
+                }
+            }
+
+            Console.WriteLine("The sum is: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
